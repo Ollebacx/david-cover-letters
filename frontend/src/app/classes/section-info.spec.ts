c1069d29da0a7a406a1a0e7bec100b9711189123
Ollebacx <dav.ollebac@gmail.com>
2020-08-26 14:31:19 +0100

    section class created

---
+++ frontend/src/app/classes/section-info.spec.ts
@@ -0,0 +1,7 @@
+import { SectionInfo } from './section-info';
+
+describe('SectionInfo', () => {
+  it('should create an instance', () => {
+    expect(new SectionInfo()).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/classes/section-info.ts
@@ -0,0 +1,7 @@
+export class SectionInfo {
+  title: string;
+  text: string;
+  bgColor: string;
+  textColor: string;
+  date: string
+}
