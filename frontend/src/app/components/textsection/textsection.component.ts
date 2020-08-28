e7b20c9f5069d68ebad8c612791a62a9a362ee5b
Ollebacx <dav.ollebac@gmail.com>
2020-08-28 14:25:12 +0100

    flytext component-animation applied and working

--- frontend/src/app/components/textsection/textsection.component.ts
+++ frontend/src/app/components/textsection/textsection.component.ts
@@ -1,9 +1,30 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 @Component({
   selector: 'app-textsection',
   templateUrl: './textsection.component.html',
-  styleUrls: ['./textsection.component.scss']
+  styleUrls: ['./textsection.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({
+        transform: 'translateX(0)'
+      })),
+      transition(':enter', [
+        style({ transform: 'translateX(-100%' }),
+        animate(1000)
+      ]),
+      transition(':leave', [
+        animate(1000, style({ transform: 'translateX(100%' }))
+      ])
+    ])
+  ]
 })
 export class TextsectionComponent implements OnInit {
   @Input() title: string;
