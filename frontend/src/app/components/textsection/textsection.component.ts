13195e2f72739e2d4750d794fd644ff1454cbd91
Ollebacx <dav.ollebac@gmail.com>
2020-08-31 20:54:01 +0100

    Addd progresbar & changd html structur. Fix scroll

--- frontend/src/app/components/textsection/textsection.component.ts
+++ frontend/src/app/components/textsection/textsection.component.ts
@@ -1,12 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { trigger } from '@angular/animations';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'app-textsection',
   templateUrl: './textsection.component.html',
   styleUrls: ['./textsection.component.scss'],
-  animations: [trigger('inOutAnimation', [])]
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ opacity: 0 }),
+            animate('0.5s ease-out',
+              style({ opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave',
+          [
+            style({ opacity: 1 }),
+            animate('0.5s ease-in',
+              style({ opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 
 export class TextsectionComponent implements OnInit {
@@ -16,15 +37,40 @@ export class TextsectionComponent implements OnInit {
   @Input() bgColor: string;
   @Input() textColor: string;
   @Input() date: string;
+  winScroll;
+  height;
+  scrolled = 0;
+  hasScroll;
+  element
+  style
 
   onScroll(event: any) {
+    console.log('scrolled');
+    this.winScroll = event.target.scrollTop;
+    this.height = event.target.scrollHeight - event.target.offsetHeight;
+    this.scrolled = (this.winScroll / this.height) * 100;
+    document.getElementById("myBar").style.width = this.scrolled + "%";
     if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight) {
       console.log("End");
     }
   }
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    console.log(this.hasScroll);
+
+    var elem = document.getElementById('body-text');
+    if (elem.scrollHeight > (window.innerHeight || document.documentElement.clientHeight)) {
+      // Bottom is out of viewport
+      this.hasScroll = true;
+      console.log(this.hasScroll);
+    }
+    else {
+      this.hasScroll = false;
+    }
+
+  }
 }
+
+
