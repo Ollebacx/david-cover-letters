d181762e0aa04175b5a0a2e95f265ac4ad312f63
Ollebacx <dav.ollebac@gmail.com>
2020-09-26 15:09:01 +0100

    contact animation

--- frontend/src/app/animations/animation.ts
+++ frontend/src/app/animations/animation.ts
@@ -37,49 +37,49 @@ export const slideInAnimation =
     //   ]),
     //   query(':enter', animateChild()),
     // ]),
-    transition('* <=> *', [
-      style({ position: 'relative' }),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
+    // transition('* <=> *', [
+    //   style({ position: 'relative' }),
+    //   query(':enter, :leave', [
+    //     style({
+    //       position: 'absolute',
+    //       top: 0,
+    //       left: 0,
+    //       width: '100%',
 
-        })
-      ], { optional: true }),
-      //
-      query(':enter', [
-        style({ transform: 'translateX({{offsetEnter}}%)', opacity: '0' })
-      ], { optional: true }),
-      //
-      query(':leave', [
-        style({ transform: 'translateX(0%)', opacity: '1' })
-      , animateChild()], { optional: true }),
-      //
-      group([
-        query(':leave', [
-          animate('700ms ease-in-out', style({ transform: 'translateX({{offsetLeave}}%)', opacity: '0' })),
+    //     })
+    //   ], { optional: true }),
+    //   //
+    //   query(':enter', [
+    //     style({ transform: 'translateX({{offsetEnter}}%)', opacity: '0' })
+    //   ], { optional: true }),
+    //   //
+    //   query(':leave', [
+    //     style({ transform: 'translateX(0%)', opacity: '1' })
+    //   , animateChild()], { optional: true }),
+    //   //
+    //   group([
+    //     query(':leave', [
+    //       animate('700ms ease-in-out', style({ transform: 'translateX({{offsetLeave}}%)', opacity: '0' })),
 
-        ], { optional: true }),
-        //
-        // query('@inOutAnimation', [
-        //   style({ transform: 'translateX(10%)', opacity: 0 }),
-        // ], { optional: true }),
-        sequence([
-          query(':enter', [
-            animate('700ms ease-in-out', style({ transform: 'translateX(0%)', opacity: '1' }))
-          ], { optional: true }),
-          // query('@inOutAnimation', [
-          //   animate('1s ease-out',
-          //     style({ transform: 'translateY(0%)', opacity: 1 }))
-          // ], { optional: true })
-        ])
-      ]),
-      //
-      query(':enter', animateChild(), { optional: true }),
-      query('@inOutAnimation', animateChild(), { optional: true }),
-    ]),
+    //     ], { optional: true }),
+    //     //
+    //     // query('@inOutAnimation', [
+    //     //   style({ transform: 'translateX(10%)', opacity: 0 }),
+    //     // ], { optional: true }),
+    //     sequence([
+    //       query(':enter', [
+    //         animate('700ms ease-in-out', style({ transform: 'translateX(0%)', opacity: '1' }))
+    //       ], { optional: true }),
+    //       // query('@inOutAnimation', [
+    //       //   animate('1s ease-out',
+    //       //     style({ transform: 'translateY(0%)', opacity: 1 }))
+    //       // ], { optional: true })
+    // //     ])
+    // //   ]),
+    // //   //
+    // //   query(':enter', animateChild(), { optional: true }),
+    // //   query('@inOutAnimation', animateChild(), { optional: true }),
+    // // ]),
     transition('* => ContactPage', [
       style({ position: 'relative' }),
       query(':enter, :leave', [
@@ -91,24 +91,13 @@ export const slideInAnimation =
         })
       ], { optional: true }),
       query(':enter', [
-        style({ transform: 'translateX(-100%)' })
-      ], { optional: true }),
+        style({ transform: 'translateY(100%)' }),
+        animate('700ms ease-in-out', style({ transform: 'translateY(0%)' })),
+        animateChild()], { optional: true }),
       query(':leave', [
-        style({ transform: 'translateX(0%)' })
-        , animateChild()], { optional: true }),
-      //
-      group([
-        query(':leave', [
-          animate('700ms ease-in-out', style({ transform: 'translateX(100%)'})),
-
-        ], { optional: true }),
-        sequence([
-          query(':enter', [
-            animate('700ms ease-in-out', style({ transform: 'translateX(0%)' }))
-          ], { optional: true })
-        ])
-      ]),
-      //
+        style({ transform: 'translateY(0%)' }),
+        animate('700ms ease-in-out', style({ transform: 'translateY(-100%)' })),
+        animateChild()], { optional: true }),
       query(':enter', animateChild(), { optional: true }),
     ]),
     transition('ContactPage => *', [
@@ -122,20 +111,20 @@ export const slideInAnimation =
         })
       ], { optional: true }),
       query(':enter', [
-        style({ transform: 'translateX(100%)' })
+        style({ transform: 'translateY(-100%)' })
       ], { optional: true }),
       query(':leave', [
-        style({ transform: 'translateX(0%)' })
+        style({ transform: 'translateY(0%)' })
         , animateChild()], { optional: true }),
       //
       group([
         query(':leave', [
-          animate('700ms ease-in-out', style({ transform: 'translateX(-100%)' })),
+          animate('700ms ease-in-out', style({ transform: 'translateY(100%)' })),
 
         ], { optional: true }),
         sequence([
           query(':enter', [
-            animate('700ms ease-in-out', style({ transform: 'translateX(0%)' }))
+            animate('700ms ease-in-out', style({ transform: 'translateY(0%)' }))
           ], { optional: true })
         ])
       ]),
