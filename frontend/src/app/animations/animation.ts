7b711c8ce7d75202ee825b76b97fec4ae1d5d0a0
Ollebacx <dav.ollebac@gmail.com>
2020-10-01 17:11:02 +0100

    contactPage animation working

--- frontend/src/app/animations/animation.ts
+++ frontend/src/app/animations/animation.ts
@@ -6,96 +6,34 @@ import {
   transition,
   query,
   animateChild,
+  group,
+  sequence
 } from '@angular/animations';
 
 export const slideInAnimation =
   trigger('routeAnimations', [
-    // transition('* <=> *', [
-    //   style({ position: 'relative' }),
-    //   query(':enter, :leave', [
-    //     style({
-    //       position: 'absolute',
-    //       top: 0,
-    //       left: 0,
-    //       width: '100%'
-    //     })
-    //   ], { optional: true }),
-    //   query(':enter', [
-    //     style({ left: '-100%' })
-    //   ], { optional: true }),
-    //   query(':leave', animateChild()),
-    //   group([
-    //     query(':leave', [
-    //       animate('300ms ease-out', style({ left: '100%' }))
-    //     ], { optional: true }),
-    //     query(':enter', [
-    //       animate('300ms ease-out', style({ left: '0%' }))
-    //     ], { optional: true })
-    //   ]),
-    //   query(':enter', animateChild()),
-    // ]),
-    // transition('* <=> *', [
-    //   style({ position: 'relative' }),
-    //   query(':enter, :leave', [
-    //     style({
-    //       position: 'absolute',
-    //       top: 0,
-    //       left: 0,
-    //       width: '100%',
-
-    //     })
-    //   ], { optional: true }),
-    //   //
-    //   query(':enter', [
-    //     style({ transform: 'translateX({{offsetEnter}}%)', opacity: '0' })
-    //   ], { optional: true }),
-    //   //
-    //   query(':leave', [
-    //     style({ transform: 'translateX(0%)', opacity: '1' })
-    //   , animateChild()], { optional: true }),
-    //   //
-    //   group([
-    //     query(':leave', [
-    //       animate('700ms ease-in-out', style({ transform: 'translateX({{offsetLeave}}%)', opacity: '0' })),
-
-    //     ], { optional: true }),
-    //     //
-    //     // query('@inOutAnimation', [
-    //     //   style({ transform: 'translateX(10%)', opacity: 0 }),
-    //     // ], { optional: true }),
-    //     sequence([
-    //       query(':enter', [
-    //         animate('700ms ease-in-out', style({ transform: 'translateX(0%)', opacity: '1' }))
-    //       ], { optional: true }),
-    //       // query('@inOutAnimation', [
-    //       //   animate('1s ease-out',
-    //       //     style({ transform: 'translateY(0%)', opacity: 1 }))
-    //       // ], { optional: true })
-    // //     ])
-    // //   ]),
-    // //   //
-    // //   query(':enter', animateChild(), { optional: true }),
-    // //   query('@inOutAnimation', animateChild(), { optional: true }),
-    // // ]),
     transition('* => ContactPage', [
       style({ position: 'relative' }),
       query(':enter, :leave', [
         style({
           position: 'absolute',
-          top: 0,
-          left: 0,
+          // top: 0,
+          // left: 0,
           width: '100%',
         })
       ], { optional: true }),
       query(':enter', [
-        style({ transform: 'translateY(100%)' }),
-        animate('700ms ease-in-out', style({ transform: 'translateY(0%)' })),
-        animateChild()], { optional: true }),
-      query(':leave', [
-        style({ transform: 'translateY(0%)' }),
-        animate('700ms ease-in-out', style({ transform: 'translateY(-100%)' })),
-        animateChild()], { optional: true }),
-      query(':enter', animateChild(), { optional: true }),
+        style({ transform: 'translateY(100%)', background: 'var(--background)' })]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('1000ms ease-in-out', style({ transform: 'translateY(-100%)' })),
+        ], { optional: true }),
+        query(':enter', [
+          animate('1300ms ease-in-out', style({ transform: 'translateY(0%)' })),
+        ]),
+        query(':enter', animateChild())
+      ])
     ]),
     transition('ContactPage => *', [
       style({ position: 'relative' }),
@@ -108,13 +46,17 @@ export const slideInAnimation =
         })
       ], { optional: true }),
       query(':enter', [
-        style({ transform: 'translateY(-100%)', background: 'var(--background)' }),
-        animate('700ms ease-in-out', style({ transform: 'translateY(0%)' })),
-        animateChild()], { optional: true }),
-      query(':leave', [
-        style({ transform: 'translateY(0%)' }),
-        animate('700ms ease-in-out', style({ transform: 'translateY(100%)' })),
-        animateChild()], { optional: true }),
+      style({ transform: 'translateY(-100%)', background: 'var(--background)' })]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('1000ms ease-in-out', style({ transform: 'translateY(100%)' })),
+        ], { optional: true }),
+        query(':enter', [
+          animate('1500ms ease-in-out', style({ transform: 'translateY(0%)' })),
+        ]),
+        query(':enter', animateChild())
+      ])
     ])
   ]);
 
