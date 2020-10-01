102bba8f6a68d7ddf45902d1b2b1cd5667d7dae3
Ollebacx <dav.ollebac@gmail.com>
2020-10-01 17:06:51 +0100

    angular default strategy

--- frontend/src/app/classes/custom-reuse-strategy.ts
+++ frontend/src/app/classes/custom-reuse-strategy.ts
@@ -7,24 +7,14 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
   shouldDetach(route: ActivatedRouteSnapshot): boolean {
     return false;
   }
-
-  store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-    this.handlers[route.routeConfig.path] = null;
-  }
-
+  store(route: ActivatedRouteSnapshot, detachedTree: DetachedRouteHandle): void { }
   shouldAttach(route: ActivatedRouteSnapshot): boolean {
-    return !!route.routeConfig && !!this.handlers[route.routeConfig.path];
+    return false;
   }
-
-  retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
-    if (!route.routeConfig) {
-      return null;
-    }
-    return this.handlers[route.routeConfig.path];
+  retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle | null {
+    return null;
   }
-
   shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-    return curr.component !== ScriptComponent;
+    return future.routeConfig === curr.routeConfig;
   }
-
 }
