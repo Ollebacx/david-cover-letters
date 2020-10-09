8870f8f8cb237c5cc43810a21b76f29911bccbf0
Ollebacx <dav.ollebac@gmail.com>
2020-10-09 18:00:55 +0100

    Contact email animation

--- frontend/src/app/components/contact-form/contact-form.component.ts
+++ frontend/src/app/components/contact-form/contact-form.component.ts
@@ -6,18 +6,51 @@ import { trigger, state, animate, transition, style } from '@angular/animations'
 @Component({
   selector: 'app-contact-form',
   animations: [
-    trigger('toggle', [
+    trigger('subject-1', [
       state('true', style({
-        opacity: 1,
-        transform: "translateY(-100px)",
+        // opacity: 1,
+        transform: "translateY(0px)",
       })),
       state('void', style({
-        opacity: 0,
-        transform: "translateY(0px)",
+        // opacity: 0,
+        transform: "translateY(200px)",
 
       })),
-      transition(':enter', animate('500ms ease-in-out')),
-      transition(':leave', animate('500ms ease-in-out'))
+      transition(':enter', animate('700ms 500ms ease-in-out')),
+      transition(':leave', animate('700ms ease-in-out'))
+    ]),
+    trigger('subject-2', [
+      state('true', style({
+        // opacity: 1,
+        transform: "translateY(0px)",
+      })),
+      state('void', style({
+        // opacity: 0,
+        transform: "translateY(200px)",
+
+      })),
+      transition(':enter', animate('700ms 1500ms ease-in-out')),
+      transition(':leave', animate('700ms ease-in-out'))
+    ]),
+    trigger('comment-1', [
+      state('true', style({
+        transform: "translateY(0px)",
+      })),
+      state('void', style({
+        transform: "translateY(200px)",
+      })),
+      transition(':enter', animate('1000ms ease-in-out')),
+      transition(':leave', animate('1000ms ease-in-out'))
+    ]),
+    trigger('comment-2', [
+      state('true', style({
+        transform: "translateY(0px)",
+      })),
+      state('void', style({
+        transform: "translateY(300px)",
+      })),
+      transition(':enter', animate('1000ms 500ms ease-in-out')),
+      transition(':leave', animate('1000ms ease-in-out'))
     ])
   ],
   templateUrl: './contact-form.component.html',
@@ -26,12 +59,9 @@ import { trigger, state, animate, transition, style } from '@angular/animations'
 export class ContactFormComponent implements OnInit {
   fullName: string = '';
   subject: string = '';
-  message: string = '';
-  FormStatus: boolean = true;
-
+  message: string = `Hola David, ¿cómo va? \n\nTe escribo para contarte que...`;
   constructor(public router: Router, private contact: ContactService) { }
 
   ngOnInit(): void {
-
   }
 }
