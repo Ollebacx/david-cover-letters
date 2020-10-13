b836e0aadbb8dca4e0a85e5d3b7756c0baa90027
Ollebacx <dav.ollebac@gmail.com>
2020-10-13 17:14:30 +0100

    Form structure, animation and logic implemented

--- frontend/src/app/components/contact-form/contact-form.component.ts
+++ frontend/src/app/components/contact-form/contact-form.component.ts
@@ -16,8 +16,8 @@ import { trigger, state, animate, transition, style } from '@angular/animations'
         transform: "translateY(200px)",
 
       })),
-      transition(':enter', animate('700ms 500ms ease-in-out')),
-      transition(':leave', animate('700ms ease-in-out'))
+      transition(':enter', animate('1500ms 500ms ease-in-out')),
+      transition(':leave', animate('1500ms ease-in-out'))
     ]),
     trigger('subject-2', [
       state('true', style({
@@ -29,8 +29,8 @@ import { trigger, state, animate, transition, style } from '@angular/animations'
         transform: "translateY(200px)",
 
       })),
-      transition(':enter', animate('700ms 1500ms ease-in-out')),
-      transition(':leave', animate('700ms ease-in-out'))
+      transition(':enter', animate('1500ms 1500ms ease-in-out')),
+      transition(':leave', animate('1500ms ease-in-out'))
     ]),
     trigger('comment-1', [
       state('true', style({
@@ -40,14 +40,15 @@ import { trigger, state, animate, transition, style } from '@angular/animations'
         transform: "translateY(200px)",
       })),
       transition(':enter', animate('1000ms ease-in-out')),
-      transition(':leave', animate('1000ms ease-in-out'))
+      transition(':leave', animate('1000ms 500ms ease-in-out'))
     ]),
     trigger('comment-2', [
       state('true', style({
         transform: "translateY(0px)",
+        width: '100%'
       })),
       state('void', style({
-        transform: "translateY(300px)",
+        transform: "translateY(200px)",
       })),
       transition(':enter', animate('1000ms 500ms ease-in-out')),
       transition(':leave', animate('1000ms ease-in-out'))
@@ -60,8 +61,28 @@ export class ContactFormComponent implements OnInit {
   fullName: string = '';
   subject: string = '';
   message: string = `Hola David, ¿cómo va? \n\nTe escribo para contarte que...`;
+  isSent: boolean = false;
+
   constructor(public router: Router, private contact: ContactService) { }
 
   ngOnInit(): void {
+  }
+  mail() {
+    if (this.subject.length > 8 && this.fullName.length > 2 && this.message.length > 50) {
+      setTimeout(() => {
+        window.location.href = `mailto:dav.ollebac@gmail.com?subject=${this.subject}&body=${this.message}%0D%0AUn saludo, %0D%0${this.fullName}.`
+        setTimeout(() => {
+          this.fullName = '';
+          this.subject = '';
+          this.message = `Hola David, ¿cómo va? \n\nTe escribo para contarte que...`;
+          this.isSent = true;
+        }, 300);
+      }, 800)
+    }
+  }
+  openLinkedIn() {
+    setTimeout(() => {
+      window.open("https://www.linkedin.com/in/david-cabello-oquendo/", '_blank');
+    }, 300);
   }
 }
