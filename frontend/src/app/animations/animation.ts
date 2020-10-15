db300b2322e4db2f9259cacc0207c2224f109147
Ollebacx <dav.ollebac@gmail.com>
2020-10-15 11:46:47 +0100

    transition page animation working

--- frontend/src/app/animations/animation.ts
+++ frontend/src/app/animations/animation.ts
@@ -12,30 +12,54 @@ import {
 
 export const slideInAnimation =
   trigger('routeAnimations', [
-    transition('* => ContactPage', [
-      style({ position: 'relative' }),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          // top: 0,
-          // left: 0,
-          width: '100%',
-        })
-      ], { optional: true }),
-      query(':enter', [
-        style({ transform: 'translateY(100%)', background: 'var(--background)' })]),
-      query(':leave', animateChild()),
-      group([
-        query(':leave', [
-          animate('1000ms ease-in-out', style({ transform: 'translateY(-100%)' })),
+      transition('* => ContactPage', [
+        style({ position: 'relative' }),
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            background: 'var(--background)'
+          })
         ], { optional: true }),
         query(':enter', [
-          animate('1300ms ease-in-out', style({ transform: 'translateY(0%)' })),
-        ]),
-        query(':enter', animateChild())
-      ])
-    ]),
-    transition('ContactPage => *', [
+          style({ transform: 'translateY(100%)' })]),
+        query(':leave', animateChild()),
+        group([
+          query(':leave', [
+            animate('1000ms ease-in-out', style({ transform: 'translateY(-100%)' })),
+          ], { optional: true }),
+          query(':enter', [
+            animate('1300ms ease-in-out', style({ transform: 'translateY(0%)' })),
+          ]),
+          query(':enter', animateChild())
+        ])
+      ]),
+      transition('ContactPage => *', [
+        style({ position: 'relative' }),
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+          })
+        ], { optional: true }),
+        query(':enter', [
+        style({ transform: 'translateY(-100%)', background: 'var(--background)' })]),
+        query(':leave', animateChild()),
+        group([
+          query(':leave', [
+            animate('1000ms ease-in-out', style({ transform: 'translateY(100%)' })),
+          ], { optional: true }),
+          query(':enter', [
+            animate('1500ms ease-in-out', style({ transform: 'translateY(0%)' })),
+          ]),
+          query(':enter', animateChild())
+        ])
+      ]),
+    transition('HomePage => ScriptPage', [
       style({ position: 'relative' }),
       query(':enter, :leave', [
         style({
@@ -46,14 +70,37 @@ export const slideInAnimation =
         })
       ], { optional: true }),
       query(':enter', [
-      style({ transform: 'translateY(-100%)', background: 'var(--background)' })]),
+        style({ transform: 'translateX(100%)' })]),
       query(':leave', animateChild()),
       group([
         query(':leave', [
-          animate('1000ms ease-in-out', style({ transform: 'translateY(100%)' })),
+          animate('1000ms ease-in-out', style({ transform: 'translateX(-100%)' })),
         ], { optional: true }),
         query(':enter', [
-          animate('1500ms ease-in-out', style({ transform: 'translateY(0%)' })),
+          animate('1300ms ease-in-out', style({ transform: 'translateX(0%)' })),
+        ]),
+        query(':enter', animateChild())
+      ])
+    ]),
+    transition('ScriptPage => HomePage', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+        })
+      ], { optional: true }),
+      query(':enter', [
+        style({ transform: 'translateX(-100%)' })]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('1000ms ease-in-out', style({ transform: 'translateX(100%)' })),
+        ], { optional: true }),
+        query(':enter', [
+          animate('1300ms ease-in-out', style({ transform: 'translateX(0%)' })),
         ]),
         query(':enter', animateChild())
       ])
